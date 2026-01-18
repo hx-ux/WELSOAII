d71898822136a616106234042092da6eff188892
jakob <itsmecettle@gmail.com>
2026-01-18 15:42:17 +0100

    added path manager

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -1,5 +1,8 @@
 pub mod color_helper;
 pub mod global_settings;
+pub mod path_manager;
+
 pub use color_helper::ColorHelpers;
 pub use global_settings::AppMode;
 pub use global_settings::GlobalSettings;
+pub use path_manager::PathManager;
