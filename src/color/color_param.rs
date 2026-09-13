0512ff0702d2213d10d8790032f1869764017d85
jakob <itsmecettle@gmail.com>
2026-09-13 21:59:23 +0200

    [REBASE] Color Palette

--- src/color/color_param.rs
+++ src/color/color_param.rs
@@ -83,13 +83,8 @@ impl ColorParam {
             return self.single_color;
         }
 
-        let palette = self.palette.as_vec();
-        if palette.is_empty() {
-            return self.single_color;
-        }
-
-        let mapped_index = clamp(index % palette.len(), 0, palette.len() - 1);
-        palette[mapped_index]
+        let palette = self.palette.as_slice();
+        palette[clamp(index % palette.len(), 0, palette.len() - 1)]
     }
 }
 
@@ -102,30 +97,3 @@ impl Default for ColorParam {
         }
     }
 }
-// impl ColorHelpers for Rgba {
-//     fn hsv_to_rgb(h: f32, s: f32, v: f32) -> (u8, u8, u8) {
-//         let c = v * s;
-//         let x = c * (1.0 - ((h / 60.0) % 2.0 - 1.0).abs());
-//         let m = v - c;
-
-//         let (r_prime, g_prime, b_prime) = if (0.0..60.0).contains(&h) {
-//             (c, x, 0.0)
-//         } else if (60.0..120.0).contains(&h) {
-//             (x, c, 0.0)
-//         } else if (120.0..180.0).contains(&h) {
-//             (0.0, c, x)
-//         } else if (180.0..240.0).contains(&h) {
-//             (0.0, x, c)
-//         } else if (240.0..300.0).contains(&h) {
-//             (x, 0.0, c)
-//         } else {
-//             (c, 0.0, x)
-//         };
-
-//         (
-//             ((r_prime + m) * 255.0) as u8,
-//             ((g_prime + m) * 255.0) as u8,
-//             ((b_prime + m) * 255.0) as u8,
-//         )
-//     }
-// }
